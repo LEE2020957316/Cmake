b89d3726644d36b1fbf65a000f530375f6968f78
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2025-03-19 17:14:54 +0000

    Add files via upload

---
+++ build/CMakeFiles/CMakeMouse.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeMouse.dir/CMakeMouse.cpp.o"
+  "CMakeFiles/CMakeMouse.dir/CMakeMouse.cpp.o.d"
+  "CMakeMouse"
+  "CMakeMouse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CMakeMouse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CMakeMouse.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CMakeMouse.
+# This may be replaced when dependencies are built.
